998275738e9762b7d5eaa7cedecd11c7a83b288f
Vinit Parakh <parakh.vinit2002@gmail.com>
2024-11-28 01:27:12 +0530

    removed errors

--- frontend/src/components/QuizDetail/index.ts
+++ frontend/src/components/QuizDetail/index.ts
@@ -1,17 +1,6 @@
 import LoadingSpinner from './LoadingSpinner';
 import NotFound from './NotFound';
 import QuizHeader from './QuizHeader';
-import PerformanceOverview from './PerformanceOverview';
 import NavigationTabs from './NavigationTabs';
-import SummarySection from './SummarySection';
-import QuestionsSection from './QuestionsSection';
 
-export {
-  LoadingSpinner,
-  NotFound,
-  QuizHeader,
-  PerformanceOverview,
-  NavigationTabs,
-  SummarySection,
-  QuestionsSection,
-};
+export { LoadingSpinner, NotFound, QuizHeader, NavigationTabs };
