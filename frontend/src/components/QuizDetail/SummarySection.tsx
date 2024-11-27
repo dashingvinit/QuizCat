0bc0c69be5343b437035e8fe33bbd7992e18451e
Vinit Parakh <parakh.vinit2002@gmail.com>
2024-11-28 01:28:52 +0530

    removed usless files

--- frontend/src/components/QuizDetail/SummarySection.tsx
+++
@@ -1,6 +0,0 @@
-function SummarySection({ quizDetail }) {
-  // Similar implementation as in the original Summary view
-  return <div>/* Render summary details here */</div>;
-}
-
-export default SummarySection;
